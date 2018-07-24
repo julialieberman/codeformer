f6e5a6b671387c0569ffa69c6501f9f367f64923
Julia Lieberman <30445374+julialieberman@users.noreply.github.com>
2018-07-24 16:45:42 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using UnityEngine.XR.WSA.WebCam;
+
+public class PhotoCapture : MonoBehaviour
+{
+    PhotoCapture photoCaptureObject = null;
+    Texture2D targetTexture = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+
+        // Create a PhotoCapture object
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
+            photoCaptureObject = captureObject;
+            CameraParameters cameraParameters = new CameraParameters();
+            cameraParameters.hologramOpacity = 0.0f;
+            cameraParameters.cameraResolutionWidth = cameraResolution.width;
+            cameraParameters.cameraResolutionHeight = cameraResolution.height;
+            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+
+            // Activate the camera
+            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
+                // Take a picture
+                photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+            });
+        });
+
+    }
+
+    void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+    {
+        // Copy the raw image data into the jpeg file 
+        if (result.success)
+        {
+            tempFileName = string.Format(@"CapturedImage{0}_n.jpg", Time.time);
+            //specify file path here
+            string filePath = System.IO.Path.Combine(Application.persistentDataPath, tempFileName);
+            tempFilePathAndName = filePath;
+            Debug.Log("Saving photo to " + filePath);
+
+            try
+            {
+                photoCaptureObject.TakePhotoAsync(filePath, PhotoCaptureFileOutputFormat.JPG, onCapturedPhotoToDisk);
+            }
+            catch (System.ArgumentException e)
+            {
+                Debug.LogError("System.ArgumentException:\n" + e.Message);
+            }
+        }
+        // Deactivate the camera
+        photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+    }
+
+
+    void onCapturedPhotoToDisk(PhotoCapture.PhotoCaptureResult result)
+    {
+        if (result.success)
+        {
+            Debug.Log("Saved Photo to disk!");
+            photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+            Debug.Log("moving " + tempFilePathAndName + " to " + picturesFolder.Path + "\\Camera Roll\\" + tempFileName);
+            File.Move(tempFilePathAndName, picturesFolder.Path + "\\Camera Roll\\" + tempFileName);
+        }
+        else
+        {
+            Debug.Log("Failed to save Photo to disk " + result.hResult + " " + result.resultType.ToString());
+        }
+    }
+    void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    {
+        // Shutdown the photo capture resource
+        photoCaptureObject.Dispose();
+        photoCaptureObject = null;
+    }
+}
